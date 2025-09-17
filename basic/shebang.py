d783edb265f884d92ac9c48c1d46b4fd08bb95d1
edibauer <edibauer@localhost.localdomain>
2025-09-16 18:11:47 -0600

    feat: basics python

---
+++ basic/shebang.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# snake_case
+# SCREAMING_SNAKE_CASE
+
+if __name__=="__main__":
+    print("im the principal module")
+else:
+    print("im not the principal module")
---
+++ basic/vars_data_types.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# vars
+ip_address = "192.168.1.1"
+port = 80
+number = 3.3
+
+
+# data tyoes
+print(ip_address)
+print(type(ip_address))
+
+print(port)
+print(type(port))
+
+print(number)
+print(type(number))
+
